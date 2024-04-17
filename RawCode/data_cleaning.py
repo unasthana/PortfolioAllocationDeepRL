a398de692ec0d8942e20776dd53fa2e4a2066e7e
unasthana <128328811+unasthana@users.noreply.github.com>
2024-04-17 19:16:34 -0400

    Create data_cleaning.py
    Script to clean data and find out dates of intersection for different stocks.

---
+++ RawCode/data_cleaning.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+import subprocess
+from line_profiler import LineProfiler
+
+def clean_data():
+
+    if not os.path.exists("/content/history.csv"):
+        script_path = '/content/OptimizedCode/data_scraping.py'
+        subprocess.run(["python", script_path])
+
+    hist = pd.read_csv("/content/history.csv")
+    hist.dropna(inplace=True)
+    hist.set_index("Date", inplace=True)
+
+    stock_tickers = pd.read_excel("/content/stock_tickers.xlsx")
+    tickers = stock_tickers["Stock_Ticker"].to_list()
+
+    common_dates = None
+
+    for ticker in tickers:
+        stock_data = hist[hist['Ticker'] == ticker]
+        
+        if len(stock_data) < 8000:
+          continue
+
+        if common_dates is None:
+          common_dates = set(stock_data.index.to_list())
+        
+        else:
+          common_dates = common_dates.intersection(set(stock_data.index.to_list()))
+
+    cleaned_data = hist.loc[list(common_dates)]
+    cleaned_data.to_csv("/content/cleaned_data.csv", index=True)
+
+if __name__ == "__main__":
+    profiler = LineProfiler()
+    profiler.add_function(clean_data)
+    profiler.run("clean_data()")
+    profiler.print_stats()
