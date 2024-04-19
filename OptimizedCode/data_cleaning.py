7fd61f27c6fe450dc3d9e843b063625986168ef5
unasthana <128328811+unasthana@users.noreply.github.com>
2024-04-18 21:05:25 -0400

    Update data_cleaning.py
    Fix bug to remove unwanted stock data of less than required number of entries.

--- OptimizedCode/data_cleaning.py
+++ OptimizedCode/data_cleaning.py
@@ -5,13 +5,9 @@ import multiprocessing as mp
 from line_profiler import LineProfiler
 
 def process_chunk(tickers_chunk, hist):
-    filtered_data = hist[hist['Ticker'].isin(tickers_chunk)]
-
-    ticker_counts = filtered_data['Ticker'].value_counts()
-    valid_tickers = ticker_counts.index[ticker_counts >= 8000]
-    filtered_data = filtered_data[filtered_data['Ticker'].isin(valid_tickers)].copy()
-
+    filtered_data = hist[hist['Ticker'].isin(tickers_chunk)].copy()
     filtered_data.reset_index(inplace=True)
+
     grouped_dates = filtered_data.groupby('Ticker')['Date'].agg(set)
     common_dates_chunk = set.intersection(*grouped_dates)
 
@@ -36,14 +32,17 @@ def clean_data():
     hist = pd.read_csv("/content/history.csv")
     hist.dropna(inplace=True)
     hist.set_index("Date", inplace=True)
-    
-    stock_tickers = pd.read_excel("/content/stock_tickers.xlsx")
-    tickers = stock_tickers["Stock_Ticker"].to_list()
 
+    ticker_counts = hist.groupby('Ticker').size()
+    valid_tickers = ticker_counts[ticker_counts >= 8000].index
+    hist = hist[hist['Ticker'].isin(valid_tickers)]
+
+    tickers = hist['Ticker'].unique()
     chunk_size = (len(tickers) // mp.cpu_count()) + 1
     common_dates = parallel_process(tickers, hist, chunk_size)
     
     cleaned_data = hist.loc[list(common_dates)]
+    cleaned_data.sort_index(inplace=True)
     cleaned_data.to_csv("/content/cleaned_data.csv", index=True)
 
 if __name__ == "__main__":
