b0f80577c04a9ba03782e2d4192cd454857cb811
unasthana <128328811+unasthana@users.noreply.github.com>
2024-04-29 21:38:10 -0400

    Update data_scraping.py
    Removed dependency on csv files for faster execution

--- OptimizedCode/data_scraping.py
+++ OptimizedCode/data_scraping.py
@@ -1,17 +1,25 @@
+import os
 import yfinance as yf
 import pandas as pd
 from line_profiler import LineProfiler
 
-def download_data():
+def scrape_data():
     stock_tickers = pd.read_excel("/content/stock_tickers.xlsx")
     tickers = stock_tickers["Stock_Ticker"].to_list()
     hist = yf.download(tickers=tickers, period="max", interval='1d')
 
     dataset = pd.concat([hist.xs(key=ticker, level=1, axis=1).assign(Ticker=ticker) for ticker in tickers])
-    dataset.to_csv("/content/history.csv", index=True)
 
-if __name__ == "__main__":
+    return dataset
+
+def run_scrape_data():
     profiler = LineProfiler()
-    profiler.add_function(download_data)
-    profiler.run("download_data()")
+    profiler.add_function(scrape_data)
+
+    profiler.enable_by_count()
+    history = scrape_data()
+    profiler.disable_by_count()
+
     profiler.print_stats()
+
+    return history
